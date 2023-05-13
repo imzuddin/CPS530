2351b248ea299343183370b87f699acec5e32abb
immyz-1 <imzuddin@gmail.com>
2023-05-13 01:10:16 -0400

    feat: implemented carousel and dropdown via jquery

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
+$(document).ready(() => {
     $('#recipeCardControls').carousel();
+
+    $(".carousel-control-prev").click(() => {
+      $("#recipeCardControls").carousel("prev");
+    });
+
+    $(".carousel-control-next").click(() => {
+      $("#recipeCardControls").carousel("next");
+    });
+
+    $('.navbar-toggler').click(() => {
+      $('.collapse').collapse("toggle");
+    });
+
   });
