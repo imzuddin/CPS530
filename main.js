e577ad700f424b792d063cb388abf9675b8b6bfc
immyz-1 <imzuddin@gmail.com>
2023-05-20 12:38:51 -0400

    feat: added rolling adverts with AJAX

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let currentIndex = 0; 
+
+let advertisementUrls = [
+  "./images/ad_one.jpg",
+  "./images/ad_two.jpg",
+  "./images/ad_three.jpg",
+  "./images/ad_four.jpg"
+]
+
+let descriptionUrls = [
+  "./components/descriptions/descriptionOne.html",
+  "./components/descriptions/descriptionTwo.html",
+  "./components/descriptions/descriptionThree.html",
+  "./components/descriptions/descriptionFour.html"
+];
+
 $(document).ready(() => {
     $('#recipeCardControls').carousel();
 
@@ -13,4 +29,49 @@ $(document).ready(() => {
       $('.collapse').collapse("toggle");
     });
 
+
   });
+
+
+  const iterImage = () => {
+    let url = advertisementUrls[currentIndex]
+    let descUrl = descriptionUrls[currentIndex]
+
+    let image = $.ajax({
+      url:url,
+      type:'GET',
+      xhrFields: {
+        responseType: 'blob'
+      },
+      success: (response) => {
+        let imageUrl = URL.createObjectURL(response);
+        
+        console.log(imageUrl)
+        $("#img-fluid").attr("src", imageUrl);
+      }, 
+      error: (response) => {
+  
+  
+        console.log(imageUrl)
+      }
+    })
+
+    let description = $.ajax({
+      url: descUrl,
+      type: "GET",
+      success: (response) => {
+        $("#description").html(response)
+      }
+    })
+  
+    if(currentIndex < advertisementUrls.length-1) {
+      currentIndex++;
+    } else {
+      currentIndex=0
+    }
+  
+  }
+
+  iterImage()
+  
+  setInterval(iterImage, 20000)
